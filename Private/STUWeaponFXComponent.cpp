75123b284bba247949cfbe2a44ba595600fe0cd2
Don Silvio <svobodacom1@gmail.com>
2024-08-11 21:10:35 +0300

    spawn the decals

--- Private/STUWeaponFXComponent.cpp
+++ Private/STUWeaponFXComponent.cpp
@@ -1,6 +1,8 @@
 // Don Silvio Copyright
 #include "STUWeaponFXComponent.h"
 #include "PhysicalMaterials/PhysicalMaterial.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/DecalComponent.h"
 
 
 USTUWeaponFXComponent::USTUWeaponFXComponent()
@@ -12,18 +14,29 @@ USTUWeaponFXComponent::USTUWeaponFXComponent()
 
 void USTUWeaponFXComponent::PlayImpactFX(const FHitResult& Hit)
 {
-	auto Effect = DefaultEffect;
+	auto ImpactData = DefaultImpactData;
 
 	if (Hit.PhysMaterial.IsValid())
 	{
 		const auto PhysMat = Hit.PhysMaterial.Get();
-		if (EffectsMap.Contains(PhysMat))
+		if (ImpactDataMap.Contains(PhysMat))
 		{
-			Effect = EffectsMap[PhysMat];
+			ImpactData = ImpactDataMap[PhysMat];
 		}
 	}
 
-	UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), Effect, Hit.ImpactPoint, Hit.ImpactNormal.Rotation());
+	// Niagara
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), ImpactData.NiagaraEffect, 
+		                               Hit.ImpactPoint, Hit.ImpactNormal.Rotation());
+
+	// Decal
+	auto DecalComponent = UGameplayStatics::SpawnDecalAtLocation(GetWorld(), ImpactData.DecalData.Material,
+		                           ImpactData.DecalData.Size, Hit.ImpactPoint, Hit.ImpactNormal.Rotation());
+
+	if (DecalComponent)
+	{
+		DecalComponent->SetFadeOut(ImpactData.DecalData.FadeOutTime, ImpactData.DecalData.FadeOutTime);
+	}
 }
 
 
--- Public/STUWeaponFXComponent.h
+++ Public/STUWeaponFXComponent.h
@@ -3,6 +3,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "STUCoreTypes.h"
 #include "STUWeaponFXComponent.generated.h"
 
 class UNiagaraSystem;
@@ -38,9 +39,9 @@ public:
 
 protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
-	UNiagaraSystem* DefaultEffect;
+	FImpactData DefaultImpactData;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
-	TMap<UPhysicalMaterial*, UNiagaraSystem*> EffectsMap;
+	TMap<UPhysicalMaterial*, FImpactData> ImpactDataMap;
 		
 };
