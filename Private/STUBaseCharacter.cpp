5bba3e7a1f520668bbc6a2bcb28a08de265777a2
Don Silvio <svobodacom1@gmail.com>
2024-08-12 11:02:45 +0300

    activate the ragdoll physics at the OnDeath function

--- Private/STUBaseCharacter.cpp
+++ Private/STUBaseCharacter.cpp
@@ -43,6 +43,7 @@ void ASTUBaseCharacter::BeginPlay()
 	check(HealthComponent);
 	check(HealthTextComponent);
 	check(GetCharacterMovement());
+	check(GetMesh());
 
 	OnHealthChanged(HealthComponent->GetHealth());
 	HealthComponent->OnDeath.AddUObject(this, &ASTUBaseCharacter::OnDeath);
@@ -124,7 +125,7 @@ void ASTUBaseCharacter::OnStopRunning()
 void ASTUBaseCharacter::OnDeath()
 {
 	UE_LOG(BaseCharacterLog, Display, TEXT("Player %s is dead"), *GetName());
-	PlayAnimMontage(DeathAnimMontage);
+	//PlayAnimMontage(DeathAnimMontage);
 
 	GetCharacterMovement()->DisableMovement();
    // destroy the actor
@@ -137,6 +138,9 @@ void ASTUBaseCharacter::OnDeath()
 
 	GetCapsuleComponent()->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
 	WeaponComponent->StopFire();
+
+	GetMesh()->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	GetMesh()->SetSimulatePhysics(true);
 }
 
 
