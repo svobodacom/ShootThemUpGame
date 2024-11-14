f15bf425cfc74402a4721838af8d3732f63ae26b
Don Silvio <svobodacom1@gmail.com>
2024-11-14 23:05:41 +0200

    Created STUHealthBarWidget class, SetHealthPercent function, override function OnHealthChanged in STUAICharacter, created UpdateHealthWidgetVisibility function in STUAICharacter

--- Private/STUAICharacter.cpp
+++ Private/STUAICharacter.cpp
@@ -4,6 +4,9 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "STUAIWeaponComponent.h"
 #include "BrainComponent.h"
+#include "STUHealthComponent.h"
+#include "Components/WidgetComponent.h"
+#include "STUHealthBarWidget.h"
 
 
 ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit)
@@ -18,6 +21,29 @@ ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit)
 		GetCharacterMovement()->bUseControllerDesiredRotation = true;
 		GetCharacterMovement()->RotationRate = FRotator(0.0f, 200.0f, 0.0f);
 	}
+
+	HealthWidgetComponent = CreateDefaultSubobject<UWidgetComponent>("HealthWidgetComponent");
+	HealthWidgetComponent->SetupAttachment(GetRootComponent());
+	HealthWidgetComponent->SetWidgetSpace(EWidgetSpace::Screen);
+	HealthWidgetComponent->SetDrawAtDesiredSize(true);
+}
+
+
+
+void ASTUAICharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	UpdateHealthWidgetVisibility(); 
+}
+
+
+
+void ASTUAICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(HealthWidgetComponent);
 }
 
 
@@ -31,4 +57,27 @@ void ASTUAICharacter::OnDeath()
 	{
 		STUController->BrainComponent->Cleanup();
 	}
+}
+
+
+
+void ASTUAICharacter::OnHealthChanged(float Health, float HealthDelta)
+{
+	Super::OnHealthChanged(Health, HealthDelta);
+
+	const auto HealthBarWidget = Cast<USTUHealthBarWidget>(HealthWidgetComponent->GetUserWidgetObject());
+	if (!HealthBarWidget) return;
+
+	HealthBarWidget->SetHealthPercent(HealthComponent->GetHealthPercent());
+}
+
+
+
+void ASTUAICharacter::UpdateHealthWidgetVisibility()
+{
+	if (!GetWorld() || !GetWorld()->GetFirstPlayerController() || !GetWorld()->GetFirstPlayerController()->GetPawnOrSpectator()) return;
+
+	const auto PlayerLocation = GetWorld()->GetFirstPlayerController()->GetPawnOrSpectator()->GetActorLocation();
+	const auto Distance = FVector::Distance(PlayerLocation, GetActorLocation());
+	HealthWidgetComponent->SetVisibility(Distance < HealthVisibilityDistance, true);
 }
